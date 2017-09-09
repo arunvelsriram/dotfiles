41756918f6d1493de2ad5c18ab1ced271e8d29d4
Arunvel Sriram <arunvelsriram@gmail.com>
2017-09-09 23:53:22 +0530

    Add oh-my-zsh custom files

---
+++ oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,11 @@
+# Git
+alias g='git'
+alias t='tig'
+
+# Vagrant
+alias v='vagrant'
+alias vst='vagrant status'
+alias vup='vagrant up'
+alias vpr='vagrant provision'
+alias vhlt='vagrant halt'
+alias vdy='vagrant destroy'
