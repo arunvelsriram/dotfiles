1d6abfa61d5f0d9c92f36778ab2dc0bd768628c7
Arunvel Sriram <arunvelsriram@gmail.com>
2017-12-14 21:38:36 +0530

    Remove unused aliases

--- oh-my-zsh/custom/tw_digital.zsh
+++
@@ -1,2 +0,0 @@
-# SSH to any machine through the bounce box eg. bounce agent09 will SSH in to goagent09
-alias "bounce=ssh -t -t -C -A bounce@bounce.webteam.thoughtworks.com 'bounce-to arunvelsriram $*'"
