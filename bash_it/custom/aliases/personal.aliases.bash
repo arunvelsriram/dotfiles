5382e634471abb0f7546c4af9f0b23dcbb4f5e39
Arunvel Sriram <arunvelsriram@gmail.com>
2018-04-24 01:19:34 +0530

    Add aliases

---
+++ bash_it/custom/aliases/personal.aliases.bash
@@ -0,0 +1,12 @@
+cite 'about-alias'
+about-alias 'my personal aliases'
+
+# Vim
+alias vimr='vimr -s'
+
+# Tig
+alias t='tig'
+
+# highlight
+alias hcat='highlight -O ansi --force'
+
---
+++ bash_it/custom/aliases/tw.aliases.bash
@@ -0,0 +1,4 @@
+cite 'about-alias'
+about-alias 'TW specific aliases'
+
+alias godir='cd $(fd --type d --max-depth 2 . ~/TW-Client/Projects | fzf --ansi)'
