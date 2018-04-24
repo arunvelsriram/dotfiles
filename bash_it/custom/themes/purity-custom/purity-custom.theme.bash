10ba3d9565951da4c9c64624822be98f9a1c98bf
Arunvel Sriram <arunvelsriram@gmail.com>
2018-04-24 22:33:02 +0530

    Update arrow styles for git info in theme

--- bash_it/custom/themes/purity-custom/purity-custom.theme.bash
+++ bash_it/custom/themes/purity-custom/purity-custom.theme.bash
@@ -7,12 +7,15 @@ SCM_THEME_PROMPT_SUFFIX="${reset_color}|"
 
 GIT_THEME_PROMPT_DIRTY=" ${bold_red}✗${normal}"
 GIT_THEME_PROMPT_CLEAN=" ${bold_green}✓${normal}"
-GIT_THEME_PROMPT_PREFIX="${reset_color}|"
-GIT_THEME_PROMPT_SUFFIX="${reset_color}|"
+GIT_THEME_PROMPT_PREFIX="${reset_color}["
+GIT_THEME_PROMPT_SUFFIX="${reset_color}]"
 
 STATUS_THEME_PROMPT_BAD="${bold_red}❯${reset_color}${normal} "
 STATUS_THEME_PROMPT_OK="${bold_green}❯${reset_color}${normal} "
 PURITY_THEME_PROMPT_COLOR="${PURITY_THEME_PROMPT_COLOR:=$blue}"
+
+SCM_GIT_AHEAD_CHAR="⇡:"
+SCM_GIT_BEHIND_CHAR="⇣:"
 
 function prompt_command() {
     local ret_status="$( [ $? -eq 0 ] && echo -e "$STATUS_THEME_PROMPT_OK" || echo -e "$STATUS_THEME_PROMPT_BAD")"
