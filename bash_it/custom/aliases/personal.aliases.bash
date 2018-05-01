b89f2ca4d4b61f133a21ccaaa816f3aaf0a91d7b
Arunvel Sriram <arunvelsriram@gmail.com>
2018-05-01 15:35:51 +0530

    Add aliases

--- bash_it/custom/aliases/personal.aliases.bash
+++ bash_it/custom/aliases/personal.aliases.bash
@@ -10,3 +10,16 @@ alias t='tig'
 # highlight
 alias hcat='highlight -O ansi --force'
 
+# ls
+if ls --color -d . &> /dev/null
+then
+  alias ls="ls --color=auto"
+elif ls -G -d . &> /dev/null
+then
+  alias ls='ls -G'        # Compact view, show colors
+fi
+
+alias sl=ls
+alias la='ls -AF'       # Compact view, show hidden
+alias ll='ls -al'
+alias l='ls -a'
