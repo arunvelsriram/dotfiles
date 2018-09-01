edf89f3306e87469e1b0e3de3c008ffac67b3663
Arunvel Sriram <arunvelsriram@gmail.com>
2018-09-01 18:50:06 +0530

    Make kube-tmuxp related functions a single plugin

--- bash_it/custom/plugins/kubeswitch.plugin.bash
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-cite about-plugin
-about-plugin 'Easily switch kubernetes context using tmuxp'
-
-function kubeswitch {
-  fd .  -t f -e yaml ~/.tmuxp --exec echo {/.} | fzf --bind 'enter:execute-silent(tmuxp load --yes {1})+abort'
-}
