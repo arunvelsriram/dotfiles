82793f5245161f13e4e2fa2b64dbd680f2cfd2ae
Arunvel Sriram <arunvelsriram@gmail.com>
2018-11-09 21:30:32 +0530

    Add func for opening GCP projects

---
+++ bash_it_custom/plugins/gcp.plugin.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# about-plugin open gcp project in browser
+
+opengcp () {
+  project=$(gcloud projects list \
+    | rg -v PROJECT_ID \
+    | awk '{print $1}' \
+    | fzf --reverse --height 20)
+  [ -z "$project" ] && return
+  open https://console.cloud.google.com/home/dashboard?project=$project
+}
