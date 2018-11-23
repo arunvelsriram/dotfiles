3b070c9bc128ce335e8fa068bd5dc86bee31346b
Arunvel Sriram <arunvelsriram@gmail.com>
2018-11-24 00:51:21 +0530

    Add comments to kube-tmuxp bash-it plugin

--- bash_it_custom/plugins/kube-tmuxp.plugin.bash
+++ bash_it_custom/plugins/kube-tmuxp.plugin.bash
@@ -3,14 +3,17 @@
 cite about-plugin
 about-plugin 'kube-tmuxp helper funtions'
 
+# switch kube contexts (or tmux sessions)
 function kubeswitch {
   fd .  -t f -e yaml ~/.tmuxp --exec echo {/.} | fzf --bind 'enter:execute-silent(tmuxp load --yes {1})+abort'
 }
 
+# kill all open sessions
 function tmux-kill-all {
     tmux list-sessions | rg -v attached | awk '{print $1}' | sed 's/://g' | xargs -I {} tmux kill-session -t {}
 }
 
+# select open sessions (using tab key) and kill them
 function tmux-kill {
     tmux list-sessions | rg -v attached | awk '{print $1}' | sed 's/://g' | fzf -m | xargs -I {} tmux kill-session -t {}
 }
