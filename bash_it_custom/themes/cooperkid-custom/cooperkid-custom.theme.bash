f91dfc815df594af46037cb577c7adb97d6942d8
Arunvel Sriram <arunvelsriram@gmail.com>
2019-03-04 17:22:08 +0530

    Show kubecontext in shell

--- bash_it_custom/themes/cooperkid-custom/cooperkid-custom.theme.bash
+++ bash_it_custom/themes/cooperkid-custom/cooperkid-custom.theme.bash
@@ -5,11 +5,12 @@ GIT_SHA_PREFIX="${blue}"
 GIT_SHA_SUFFIX="${reset_color}"
 
 function prompt() {
-    local workdir="${green}\w${reset_color}"
-    local git_branch="${cyan}$(scm_prompt_info)${reset_color}"
-    local prompt_char="${purple}>_${reset_color} "
+  local workdir="${green}\w${reset_color}"
+  local git_branch="${cyan}$(scm_prompt_info)${reset_color}"
+  local kubecontext="${blue}$(kube-tmux)${reset_color}"
+  local prompt_char="${purple}>_${reset_color} "
 
-    PS1="\n${workdir} ${git_branch}\n${prompt_char}"
+  PS1="\n${workdir} ${kubecontext}${git_branch}\n${prompt_char}"
 }
 
 safe_append_prompt_command prompt
