f487c44b2f02ab76f635df444101a23f6acfb18e
Arunvel Sriram <arunvelsriram@gmail.com>
2019-04-11 23:12:48 +0530

    bash-it plugin to get ClusterIP CIDR range for all K8s clusters

--- bash_it_custom/plugins/gcp.plugin.bash
+++ bash_it_custom/plugins/gcp.plugin.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-about-plugin 'open gcp project in browser'
+about-plugin 'GCP and gcloud CLI functions'
 
 opengcp () {
   project=$(gcloud projects list \
@@ -9,4 +9,12 @@ opengcp () {
     | fzf --reverse --height 20)
   [ -z "$project" ] && return
   open https://console.cloud.google.com/home/dashboard?project=$project
+}
+
+
+cluster_ip_cidr() {
+  gcloud projects list \
+    | tail -n +2 \
+    | awk -v q=\' '{ printf "gcloud --project %s container clusters list --format json | jq -rc %s.[] | \"\\(.name + \"\t\" + .clusterIpv4Cidr)\"%s\n", $1, q, q }' \
+    | bash
 }
