fb34ae4fd3558810dec99a691d36fbdcafbcb9a2
Arunvel Sriram <arunvelsriram@gmail.com>
2023-08-11 01:37:24 +0530

    Add terraform, terragrunt cleanup and prompt aliases

--- oh-my-zsh-custom/plugins/personal/personal.plugin.zsh
+++ oh-my-zsh-custom/plugins/personal/personal.plugin.zsh
@@ -72,4 +72,35 @@ alias ping="prettyping --nolegend"
 alias top="htop"
 
 # github dir
-alias gh='cd $(fd --max-depth 2 . ~/github/ | fzf)'
+alias gh='cd $(fd --max-depth 2 . ~/github/ | fzf --height=20 --ansi --reverse)'
+
+# fzf secret files and sops decrypt
+alias sopsdecrypt='sops -d $(fd "secrets.yaml|terraform-sa.json" | fzf --height=20 --ansi --reverse)'
+
+# yes/no prompt
+yesno() {
+    message=${1:="Do you want to continue ?"}
+    read "response?$message [y/n] "
+    response=${response:l}
+    echo $response
+}
+
+# find and clean .terragrunt-cache directories
+tgcacheclean() {
+    local files=$(fd -HI .terragrunt-cache)
+    local response
+    [ -z "$files" ] && echo "Nothing to clean" && return
+    echo $files
+    response=$(yesno "Delete All ?")
+    [ $response = "y" ] && rm -rfv $(echo $files | xargs)
+}
+
+# find and clean local tfstate files
+tfstateclean() {
+    local files=$(fd -HI terraform.tfstate)
+    local response
+    [ -z "$files" ] && echo "Nothing to clean" && return
+    echo $files
+    response=$(yesno "Delete All ?")
+    [ $response = "y" ] && rm -rfv $(echo $files | xargs)
+}
