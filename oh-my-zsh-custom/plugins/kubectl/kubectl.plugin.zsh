0de5d4458aab3823ea8cbd009dd7f4ae9af99fe2
Arunvel Sriram <arunvelsriram@gmail.com>
2023-08-24 01:16:24 +0530

    Update kshell script to consider only yaml files

--- oh-my-zsh-custom/plugins/kubectl/kubectl.plugin.zsh
+++ oh-my-zsh-custom/plugins/kubectl/kubectl.plugin.zsh
@@ -20,8 +20,9 @@ alias kns='kubens'
 # fzf and activate kube context using individual kubeconfig files
 # USAGE: kshell <env>
 kshell() {
-  local env=${1:-local}
-  local commands="export KUBECONFIG=${HOME}/.kube/configs/${env}/$(ls ${HOME}/.kube/configs/${env} | fzf --height=20 --ansi --reverse)"
+  local env=${1:-$(ls ${HOME}/.kube/configs | fzf --height=20 --ansi --reverse)}
+  local kubeconfig="${HOME}/.kube/configs/${env}/$(echo $(fd yaml ${HOME}/.kube/configs/${env} --exec basename {} | fzf --height=20 --ansi --reverse))"
+  local commands="export KUBECONFIG=${kubeconfig}"
   echo "${commands}"
   eval "${commands}"
 }
@@ -31,6 +32,6 @@ split-local-kubeconfigs() {
   local output_dir=~/.kube/configs/local
   for k in $(kubectl config get-contexts --kubeconfig ~/.kube/config -o name | grep 'kind-'); do
     mkdir -p ${output_dir}
-    kubectl config view --kubeconfig ~/.kube/config --context=${k} --minify --flatten > ${output_dir}/${k}
+    kubectl config view --kubeconfig ~/.kube/config --context=${k} --minify --flatten > ${output_dir}/${k}.yaml
   done
 }
