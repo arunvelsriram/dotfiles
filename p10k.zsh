fffdf9c4633b0f9d69ae7a0707461d05c803020a
Arunvel Sriram <arunvelsriram@gmail.com>
2023-08-25 09:30:48 +0530

    Show prompt for proxy

--- p10k.zsh
+++ p10k.zsh
@@ -109,7 +109,7 @@
     newline
     # ip                    # ip address and bandwidth usage for a specified network interface
     # public_ip             # public IP address
-    # proxy                 # system-wide http/https/ftp proxy
+    proxy                 # system-wide http/https/ftp proxy
     # battery               # internal battery
     # wifi                  # wifi speed
     # example               # example user-defined segment (see prompt_example function below)
