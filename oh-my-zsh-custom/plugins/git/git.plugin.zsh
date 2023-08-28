c6ad39a3e0dbd036104a59485afc145d04bf08c9
Arunvel Sriram <arunvelsriram@gmail.com>
2023-08-29 00:43:15 +0530

    Add custom git zsh plugin

---
+++ oh-my-zsh-custom/plugins/git/git.plugin.zsh
@@ -0,0 +1,33 @@
+alias g='git'
+
+alias gst='git status'
+alias gss='git status --short'
+
+alias gdiff='git diff'
+alias gdiffs='git diff --staged'
+
+alias ga='git add'
+alias gaa='git add --all'
+alias gap='git add --patch'
+alias greset='git reset'
+
+alias gcm='git commit'
+alias gcma='git commit'
+alias gcmsg='git commit --message'
+alias gcmsga='git commit --amend --message'
+
+alias glog='git log --oneline --decorate --graph'
+alias gloga='git log --oneline --decorate --graph --all'
+
+alias gpr='git pull --rebase'
+alias gpu='git push'
+
+alias gbr='git branch'
+alias gco='git checkout'
+alias gcb='git checkout -b'
+
+alias grbi='git rebase --interactive'
+
+alias gstash='git stash'
+alias gstashs='git stash --staged'
+alias gstashu='git stash -u'
