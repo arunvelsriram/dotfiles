03613a8605b0bd73123a9c270c5b8544ea52155a
Arunvel Sriram <6568319+arunvelsriram@users.noreply.github.com>
2026-05-25 22:26:36 +0530

    Add more functions to azure plugin

--- oh-my-zsh-custom/plugins/azure/azure.plugin.zsh
+++ oh-my-zsh-custom/plugins/azure/azure.plugin.zsh
@@ -3,6 +3,19 @@ function findsubscription() {
   az account list | jq -r ".[] | [.id, .name] | @tsv" | fzf --height=20 --ansi --reverse --query="$search_term"
 }
 
+function setsubscription() {
+  local subscription_payload=$(_azure_select_subscription "$1")
+  if [ $? -ne 0 ]; then
+    return 1
+  fi
+
+  local subscription_id=$(echo "$subscription_payload" | awk -F'|' '{print $1}')
+  local subscription_name=$(echo "$subscription_payload" | awk -F'|' '{print $2}')
+
+  echo "Setting subscription to: $subscription_name"
+  az account set --subscription "$subscription_id"
+}
+
 function _azure_select_subscription() {
   local search_term="$1"
   echo "Select an Azure subscription:" >&2
@@ -43,7 +56,7 @@ function findaks() {
   local subscription_id=$(echo "$subscription_payload" | awk -F'|' '{print $1}')
   local subscription_name=$(echo "$subscription_payload" | awk -F'|' '{print $2}')
 
-  echo "Selected subscription: $subscription_name"
+  echo "Selected subscription: $subscription_name, $subscription_id"
 
   local cluster_payload=$(_azure_select_aks_cluster "$subscription_id")
   local cluster_name=$(echo "$cluster_payload" | awk -F'|' '{print $1}')
@@ -90,4 +103,29 @@ function aksbrowse() {
 function azrelogin() {
   az logout
   az login
+}
+
+function akscommand() {
+  if [ -z "$1" ]; then
+    echo "Usage: akscommand <command> [additional az aks command invoke args...]" >&2
+    echo "Example: akscommand 'kubectl get pod'" >&2
+    echo "Example: akscommand 'kubectl apply -f manifest.yaml' --file manifest.yaml" >&2
+    return 1
+  fi
+
+  local context=$(kubectl config current-context)
+  local resource_group=$(echo "$context" | awk -F'/' '{print $2}')
+  local cluster_name=$(echo "$context" | awk -F'/' '{print $3}')
+
+  if [ -z "$resource_group" ] || [ -z "$cluster_name" ]; then
+    echo "Could not extract resource group and cluster name from kubecontext: $context" >&2
+    echo "Expected format: subscription/resource-group/cluster-name" >&2
+    return 1
+  fi
+
+  local command="$1"
+  shift
+
+  echo "Cluster: $cluster_name | Resource Group: $resource_group"
+  az aks command invoke --resource-group "$resource_group" --name "$cluster_name" --command "$command" "$@"
 }
